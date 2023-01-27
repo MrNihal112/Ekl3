387d3e7be573e4aa3aca40e5b73666ca36d1ba11
gokdelenler <106874399+gokdelenler@users.noreply.github.com>
2023-01-27 22:54:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ if not os.path.exists(f"Users/5452854503/phone.csv"):
 if not os.path.exists('data.csv'):
     open("data.csv","w")
 # BybossTR gereksimler  
-APP_ID =  "9858555"
-API_HASH = "d8eb215f18ec00216c83676375dc1d60"
+APP_ID =  "18716563"
+API_HASH = "1efc7a4166d018f79c3e62a03c8bf1cb"
 BOT_TOKEN = "5704165754:AAFVZvF-0f4HdHf3mxXq3Pn2YakE2fTGBKE"
 UPDATES_CHANNEL = "iyiolmayolunda"
 OWNER = [1940508871]
